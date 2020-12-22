afd0a34e04343cb45902caf870c4aa5e0c137a61
eric <erkearney>
2020-12-22 16:18:39 -0700

    Remove item.py

--- item.py
+++
@@ -1,9 +0,0 @@
-class Item:
-    def __init__(self, name, num_produced, materials=[]):
-        self.name = name
-        self.num_produced = num_produced
-        self.materials = materials
-
-
-    def print_item(self):
-        print("{}, yields: {}, materials: {}".format(self.name, self.num_produced, self.materials))
